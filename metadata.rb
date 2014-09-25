e27581b9d66031cbbae5b1da7051e4d4c9b7295a
laboshinl <laboshinl@laboshinl.stu.neva.ru>
2014-09-25 15:30:20 +0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'openstack'
+maintainer       'Leonid Laboshin'
+maintainer_email 'laboshinl@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures openstack'
+long_description 'Installs/Configures openstack'
+version          '0.9.0'
+
