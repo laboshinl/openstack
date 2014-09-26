3ca6931bc3be929bdeb8fcbaf7b251091fefed87
laboshinl <laboshinl@laboshinl.stu.neva.ru>
2014-09-27 02:55:00 +0400

    fix ntp

--- recipes/controller.rb
+++ recipes/controller.rb
@@ -7,7 +7,6 @@
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
-include_recipe "centos_cloud::ntp-server"
 include_recipe "centos_cloud::rabbitmq-server"
 #core
 include_recipe "centos_cloud::keystone"
