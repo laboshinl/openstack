a866b2afaf53f1e912f1f3267cf722d7fbb726e8
laboshinl <laboshinl@laboshinl.stu.neva.ru>
2014-10-07 14:57:09 +0400

    speed up converge with custom box

--- test/integration/default/serverspec/ntp_service_spec.rb
+++ test/integration/default/serverspec/ntp_service_spec.rb
@@ -1,26 +1,23 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
+set :backend, :exec
 
 describe "ntpd" do
 
   it "is installed" do
-    expect(package("ntpd")).to be_installed
+    expect(package("ntp")).to be_installed
+  end
+  
+  it "is enabled" do
+    expect(service("ntpd")).to be_enabled
+  end
+  
+  it "is running" do
+    expect(service("ntpd")).to be_running
   end
   
   it "is listening on port 123" do
     expect(port(123)).to be_listening
-  end
-
-  it "running" do
-    expect(service("ntpd")).to be_running
   end
 
 end
